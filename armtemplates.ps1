678548b771e6f084eb89c6bc8f3c0f15a6637549
QuickCloudLearner <ali.uib@gmail.com>
2021-02-20 12:38:33 +0100

    na

---
+++ armtemplates.ps1
@@ -0,0 +1,15 @@
+﻿### deploy an empty arm template
+
+
+$projectName = Read-Host -Prompt "Enter a project name that is used to generate resource and resource group names"
+$resourceGroupName = "${projectName}rg"
+
+New-AzResourceGroup `
+  -Name $resourceGroupName `
+  -Location "east US"
+
+  New-AzResourceGroupDeployment `
+  -Name DeployLocalTemplate  `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile d:\workspace\azuredeploy.json `
+  -DeploymentDebugLogLevel All
