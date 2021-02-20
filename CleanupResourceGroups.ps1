9fab92fcb23cb5c4571d4fbbb692ffb66a082dff
QuickCloudLearner <ali.uib@gmail.com>
2021-02-20 16:50:35 +0100

    na

--- CleanupResourceGroups.ps1
+++ CleanupResourceGroups.ps1
@@ -1,3 +1,10 @@
 ﻿
 
-Get-AzResourceGroup | Select-Object ResourceGroupName
+#Use a filter to select resource groups by substring
+$filter = 'az303'
+ 
+#Find Resource Groups by Filter -> Verify Selection
+Get-AzResourceGroup | ? ResourceGroupName -match $filter | Select-Object ResourceGroupName
+ 
+#Async Delete ResourceGroups by Filter. Uncomment the following line if you understand what you are doing. :-)
+Get-AzResourceGroup | ? ResourceGroupName -match $filter | Remove-AzResourceGroup -AsJob -Force
