fccc57658b53ee9f967277c470543bf678d2983d
QuickCloudLearner <ali.uib@gmail.com>
2021-02-20 11:58:29 +0100

    first arm template

---
+++ template_deployment.ps1
@@ -0,0 +1,50 @@
+﻿Connect-AzAccount
+
+$projectName = Read-Host -Prompt "Enter a project name that is used to generate resource and resource group names"
+$resourceGroupName = "${projectName}rg"
+
+New-AzResourceGroup `
+  -Name $resourceGroupName `
+  -Location "Central US"
+
+
+$projectName = Read-Host -Prompt "Enter the same project name"
+
+$templateFile = Read-Host -Prompt "Enter the template file path and file name"
+
+
+$resourceGroupName = "${projectName}rg"
+
+New-AzResourceGroupDeployment `
+  -Name DeployLocalTemplate `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $templateFile `
+  -projectName $projectName `
+  -verbose
+
+
+
+  New-AzResourceGroupDeployment `
+  -Name DeployLocalTemplate  `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile d:\workspace\azuredeploy.json `
+  -DeploymentDebugLogLevel All
+
+  /////////////////////////////////////////////////////////////////////////////
+
+
+
+  
+  
+$projectName = Read-Host -Prompt "Enter a project name that is used to generate resource and resource group names"
+$resourceGroupName = "${projectName}rg"
+
+New-AzResourceGroup `
+  -Name $resourceGroupName `
+  -Location "Central US"
+
+  New-AzResourceGroupDeployment `
+  -Name DeployLocalTemplate  `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile d:\workspace\azuredeploy.json `
+  -DeploymentDebugLogLevel All
